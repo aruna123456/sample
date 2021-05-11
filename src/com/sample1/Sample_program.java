6a6264eea4c0701da74675151a67f9b75bc64400
aruna123456 <shreeyuvan1122@gmail.com>
2021-05-12 00:09:16 +0530

    sample program complete

---
+++ src/com/sample1/Sample_program.java
@@ -0,0 +1,24 @@
+package com.sample1;
+
+
+public class Sample_program {
+	
+	public static void main(String[] args) {
+		
+	 
+		int a=10;
+		int b=20;
+		int c=a+b;
+		
+		System.out.println("click the element:"+c);
+
+	}
+ 
+			
+			
+		
+
+	}
+
+
+
